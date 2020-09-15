0da9d99ca8ae11725e0289efd8318a6748bd669a
Diana Grams <deegrams221@gmail.com>
2020-09-15 15:35:55 -0600

    creating placeholders for content section

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function Content() {
   return (
-    <div className='site-description'>
+    <div className='content'>
       <p>Small hobby breeder of cockatiels and green cheek conures</p>
+      <img
+        src={require('../images/placeholder-images-image_large.webp')}
+        alt='K & B Aviary Logo'
+        width='575px'
+      />
     </div>
   );
 }
