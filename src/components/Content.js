68e5595b543654855f3d019cbd7935efe6b89e88
Diana Grams <deegrams221@gmail.com>
2020-09-16 14:02:13 -0600

    form functionality working

--- src/components/Content.js
+++ src/components/Content.js
@@ -80,48 +80,76 @@ export default function Content() {
               onClose={handleClose}
               aria-labelledby='form-dialog-title'
             >
-              <DialogTitle id='form-dialog-title'>CONTACT US</DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  Enter your name, email, and a quick message.
-                </DialogContentText>
-                <DialogContentText>
-                  We will contact you shortly.
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  margin='dense'
-                  id='name'
-                  label='Name'
-                  type='text'
-                  placeholder='Enter Name Here'
-                  fullWidth
-                />
-                <TextField
-                  margin='dense'
-                  id='name'
-                  label='Email Address'
-                  type='email'
-                  placeholder='Enter Email Here'
-                  fullWidth
-                />
-                <TextField
-                  margin='dense'
-                  id='message'
-                  label='Message'
-                  type='text'
-                  placeholder='Enter Message Here'
-                  fullWidth
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color='default'>
-                  Cancel
-                </Button>
-                <Button onClick={handleClose} color='default'>
-                  Submit
-                </Button>
-              </DialogActions>
+              <form
+                action='https://formsubmit.co/deegrams221@gmail.com'
+                method='POST'
+                autoComplete='off'
+              >
+                <div className='form-group'>
+                  <div className='form-row'>
+                    <DialogTitle id='form-dialog-title'>CONTACT US</DialogTitle>
+                    <DialogContent>
+                      <DialogContentText>
+                        Enter your name, email, and a quick message.
+                      </DialogContentText>
+                      <DialogContentText>
+                        We will contact you shortly.
+                      </DialogContentText>
+                    </DialogContent>
+                    <div className='col'>
+                      <TextField
+                        autoFocus
+                        margin='dense'
+                        id='name'
+                        type='text'
+                        name='name'
+                        className='form-control'
+                        label='Name'
+                        placeholder='Enter Name Here'
+                        required
+                      />
+                    </div>
+                    <div className='col'>
+                      <TextField
+                        margin='dense'
+                        id='email'
+                        className='form-control'
+                        name='email'
+                        label='Email Address'
+                        type='email'
+                        placeholder='Enter Email Here'
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='form-group'>
+                  <TextField
+                    margin='dense'
+                    id='message'
+                    className='form-control'
+                    name='message'
+                    label='Message'
+                    type='text'
+                    placeholder='Enter Message Here'
+                    required
+                  />
+                </div>
+                <DialogActions>
+                  <Button onClick={handleClose} color='default'>
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={handleClose}
+                    color='default'
+                    type='submit'
+                    value='Send Message'
+                    className='btn btn-lg btn-dark btn-block'
+                  >
+                    Submit
+                  </Button>
+                </DialogActions>
+              </form>
             </Dialog>
           </div>
         </div>
