5dc7a86628de78718c3e043257c9e3959e83a32c
Diana Grams <deegrams221@gmail.com>
2020-09-16 14:56:05 -0600

    updted forms to be DRY

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,7 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
-} from '@material-ui/core';
 import React from 'react';
+import Form from './Form';
 
 export default function Content() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div className='content'>
       <p>Small hobby breeder of cockatiels and green cheek conures</p>
@@ -66,91 +48,8 @@ export default function Content() {
             </h3>
           </div>
 
-          <div className='contact-btn'>
-            <Button
-              className='button'
-              variant='outlined'
-              color='default'
-              onClick={handleClickOpen}
-            >
-              CONTACT US
-            </Button>
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby='form-dialog-title'
-            >
-              <form
-                action='https://formsubmit.co/deegrams221@gmail.com'
-                method='POST'
-                autoComplete='off'
-              >
-                <div className='form-group'>
-                  <div className='form-row'>
-                    <DialogTitle id='form-dialog-title'>CONTACT US</DialogTitle>
-                    <DialogContent>
-                      <DialogContentText>
-                        Enter your name, email, and a quick message.
-                      </DialogContentText>
-                      <DialogContentText>
-                        We will contact you shortly.
-                      </DialogContentText>
-                    </DialogContent>
-                    <div className='col'>
-                      <TextField
-                        autoFocus
-                        margin='dense'
-                        id='name'
-                        type='text'
-                        name='name'
-                        className='form-control'
-                        label='Name'
-                        placeholder='Enter Name Here'
-                        required
-                      />
-                    </div>
-                    <div className='col'>
-                      <TextField
-                        margin='dense'
-                        id='email'
-                        className='form-control'
-                        name='email'
-                        label='Email Address'
-                        type='email'
-                        placeholder='Enter a Valid Email Here'
-                        required
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className='form-group'>
-                  <TextField
-                    margin='dense'
-                    id='message'
-                    className='form-control'
-                    name='message'
-                    label='Message'
-                    type='text'
-                    placeholder='Enter Message Here'
-                    required
-                  />
-                </div>
-                <DialogActions>
-                  <Button onClick={handleClose} color='default'>
-                    Cancel
-                  </Button>
-                  <Button
-                    onClick={handleClose}
-                    color='default'
-                    type='submit'
-                    value='Send Message'
-                    className='btn btn-lg btn-dark btn-block'
-                  >
-                    Submit
-                  </Button>
-                </DialogActions>
-              </form>
-            </Dialog>
+          <div className='contact-button'>
+            <Form />
           </div>
         </div>
       </div>
---
+++ src/components/Form.js
@@ -0,0 +1,111 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from '@material-ui/core';
+import React from 'react';
+
+export default function Form() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className='contact-btn'>
+      <Button
+        className='button'
+        color='default'
+        variant='outlined'
+        onClick={handleClickOpen}
+      >
+        CONTACT US
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='form-dialog-title'
+      >
+        <form
+          action='https://formsubmit.co/deegrams221@gmail.com'
+          method='POST'
+          autoComplete='off'
+        >
+          <div className='form-group'>
+            <div className='form-row'>
+              <DialogTitle id='form-dialog-title'>CONTACT US</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Enter your name, email, and a quick message.
+                </DialogContentText>
+                <DialogContentText>
+                  We will contact you shortly.
+                </DialogContentText>
+              </DialogContent>
+              <div className='col'>
+                <TextField
+                  autoFocus
+                  margin='dense'
+                  id='name'
+                  type='text'
+                  name='name'
+                  className='form-control'
+                  label='Name'
+                  placeholder='Enter Name Here'
+                  required
+                />
+              </div>
+              <div className='col'>
+                <TextField
+                  margin='dense'
+                  id='email'
+                  className='form-control'
+                  name='email'
+                  label='Email Address'
+                  type='email'
+                  placeholder='Enter a Valid Email Here'
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className='form-group'>
+            <TextField
+              margin='dense'
+              id='message'
+              className='form-control'
+              name='message'
+              label='Message'
+              type='text'
+              placeholder='Enter Message Here'
+              required
+            />
+          </div>
+          <DialogActions>
+            <Button onClick={handleClose} color='default'>
+              Cancel
+            </Button>
+            <Button
+              onClick={handleClose}
+              color='default'
+              type='submit'
+              value='Send Message'
+              className='btn btn-lg btn-dark btn-block'
+            >
+              Submit
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+}
