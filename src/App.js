9233f9ac8422dea6f570ce11e71812b8e691934e
Diana Grams <deegrams221@gmail.com>
2020-09-28 16:08:00 -0600

    fixed margin on forms

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './App.css';
 
 export default function App() {
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
       <Header />
       <Content />
       <Footer />
-    </React.Fragment>
+    </>
   );
 }
