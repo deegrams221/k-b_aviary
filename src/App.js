d34363280045b2a923fb883723a452a7d6d5a1b5
Diana Grams <deegrams221@gmail.com>
2021-01-13 11:57:23 -0700

    working on admin login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
-import AccountPage from '../Account';
-import AdminPage from '../Admin';
-import HomePage from '../Home';
-import LandingPage from '../Landing';
-import Navigation from '../Navigation';
-import PasswordForgetPage from '../PasswordForget';
-import { withAuthentication } from '../Session';
-import SignInPage from '../SignIn';
-import SignUpPage from '../SignUp';
+import AdminInventoryPage from './components/admin-pages/AdminInventoryPage';
+import AdminLogin from './components/admin-pages/AdminLogin';
+import * as ROUTES from './components/constants/routes';
+import Home from './components/landingpage/Home';
 
 const App = () => (
   <Router>
     <div>
-      <Navigation />
-
-      <hr />
-
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route
-        exact
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      />
-      <Route exact path={ROUTES.HOME} component={HomePage} />
-      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={AdminLogin} />
+      <Route exact path={ROUTES.HOME} component={Home} />
+      <Route exact path={ROUTES.ADMIN} component={AdminInventoryPage} />
     </div>
   </Router>
 );
 
-export default withAuthentication(App);
+export default App;
